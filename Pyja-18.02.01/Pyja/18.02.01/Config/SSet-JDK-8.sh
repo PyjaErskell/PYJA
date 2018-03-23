64500cc3efab346e3c5df5480fa65163b460913b
PyjaErskell <36176752+PyjaErskell@users.noreply.github.com>
2018-03-23 12:25:14 +0900

    SSet-JDK-8

--- Pyja-18.02.01/Pyja/18.02.01/Config/SSet-JDK-8.sh
+++ Pyja-18.02.01/Pyja/18.02.01/Config/SSet-JDK-8.sh
@@ -1,4 +1,5 @@
 export SC_J8_HM=/Library/Java/JavaVirtualMachines/jdk1.8.0_162.jdk/Contents/Home
 export SC_J8_X_FN=$SC_J8_HM/bin/java
 export SC_J8C_X_FN=$SC_J8_HM/bin/javac
+export SC_J8H_X_FN=$SC_J8_HM/bin/javah
 export SC_J8R_X_FN=$SC_J8_HM/bin/jar
