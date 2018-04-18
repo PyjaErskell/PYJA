aa9b9220162dfdd6e1eb3ad7f3b770c02feaab79
Pyja Erskell <pyja.erskell@gmail.com>
2018-04-18 10:11:20 +0900

    SSet-Anaconda3-5.1.0.sh

---
+++ Pyja-18.02.01/Pyja/18.02.01/Config/SSet-Anaconda3-5.1.0.sh
@@ -0,0 +1,5 @@
+export SC_PYTHON_HM=/QUCK/Anaconda3/5.1.0/anaconda3
+export SC_PYTHON_EXE_FN=$SC_PYTHON_HM/bin/python
+export QT_QPA_PLATFORM_PLUGIN_PATH=$SC_PYTHON_HM/plugins/platforms
+export SC_JEP_PN=$SC_PYTHON_HM/lib/python3.6/site-packages/jep
+export SC_JEP_JAR_FN=$SC_JEP_PN/jep-3.7.1.jar
