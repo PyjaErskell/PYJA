fe5d92f0e7d02423c6786eb6a7faeee36ca75c0d
PyjaErskell <pyja.erskell@gmail.com>
2018-06-01 15:48:00 +0900

    PyQt S01-Wiggly.py

---
+++ Pyja-18.02.01/Pyja/18.02.01/Study/PyQt/18.06.01-Henshaw/S01-Wiggly.py
@@ -0,0 +1,457 @@
+#
+# Python (Global)
+#
+
+import sys
+
+sys.dont_write_bytecode = True
+
+from datetime import datetime
+
+GC_ST = datetime.now () # (G)lobal (C)onstant
+
+GC_PYJA_NM = 'Pyja'
+
+GC_PJYA_CEN = 20
+GC_PYJA_YEA = 18
+GC_PYJA_MON =  2
+GC_PYJA_DAY =  1
+
+GC_PYJA_CD = f'{GC_PJYA_CEN:02d}{GC_PYJA_YEA:02d}.{GC_PYJA_MON:02d}.{GC_PYJA_DAY:02d}' # Pyja creation date
+GC_PYJA_VR = f'{GC_PYJA_YEA:02d}.{GC_PYJA_MON:02d}.{GC_PYJA_DAY:02d}'                  # Pyja version with fixed length 8
+GC_PYJA_V2 = f'{GC_PYJA_YEA}.{GC_PYJA_MON}.{GC_PYJA_DAY}'                              # Pyja version without leading zero
+
+GC_PYJA_VER_MAJ = GC_PYJA_YEA # Major 
+GC_PYJA_VER_MIN = GC_PYJA_MON # Minor
+GC_PYJA_VER_PAT = GC_PYJA_DAY # Patch
+
+GC_PYJA_RT_SYM = '@`'
+GC_PYJA_HM_SYM = '@~'
+GC_MILO_PN_SYM = '@!'
+
+GC_EC_NONE     = -200
+GC_EC_SHUTDOWN = -199
+GC_EC_SUCCESS  = 0
+GC_EC_ERROR    = 1
+
+from PyQt5 import QtCore
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import QObject
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication
+
+GC_PYQT_VR = QtCore .qVersion ()
+GC_QAPP = QApplication ([])
+
+import os
+
+GC_FOSA = os.sep     # (fo)lder (s)ep(a)rator
+GC_PASA = os.pathsep # (pa)th (s)ep(a)rator
+
+from collections import namedtuple
+import ctypes
+import getpass
+import inspect
+import logging
+import platform
+import psutil
+import shutil
+import time
+import tkinter
+import traceback
+import types
+
+GC_PYTHON_VR = platform .python_version ()
+
+GC_TOTAL_CPU = psutil.cpu_count ()
+GC_TOTAL_MEMORY = psutil.virtual_memory () .total
+GC_HOST_NM = platform.node()
+GC_CUSR = getpass.getuser ()
+GC_THIS_PID = os.getpid ()
+
+def gf_os_env (x_key) : return os.environ [x_key] # (g)lobal (f)unction
+def gf_str_is_valid_date ( x_str, x_format ) :
+  try : time .strptime ( x_str, x_format )
+  except ValueError : return False
+  else : return True
+def gf_rm_px ( x_str, x_px ) : # px : prefix
+  return x_str [ len (x_px) : ] if x_str .startswith (x_px) else x_str
+
+GC_OS_ENV_PATHS = gf_os_env ('SC_PATH') .split (GC_PASA)
+
+def gf_ap (x_it) : # ap -> (a)bs(p)ath, x -> parameter
+  fu_trans = str.maketrans ( '\\','/' ) # (f)unction val(u)e
+  fu_ap = os.path.abspath ( x_it .translate (fu_trans) )
+  return fu_ap .translate (fu_trans)
+def gf_fn (x_fn) : # (f)ile (n)ame
+  fu_trans = str.maketrans ( '\\','/' )
+  fu_abs_fn = os.path.abspath ( x_fn .translate (fu_trans) )
+  return fu_abs_fn .translate (fu_trans)
+def gf_jn (x_fn) : # (j)ust (n)ame
+  fu_fn = gf_fn (x_fn)
+  return os.path.splitext ( os.path.basename (fu_fn) ) [0]
+def gf_xn (x_fn) : # e(x)tension (n)ame
+  fu_split = gf_bn (x_fn) .split ('.')
+  return fu_split [-1] if len (fu_split) > 1 else ''
+def gf_bn (x_fn) : # (b)ase (n)ame
+  fu_fn = gf_fn (x_fn)
+  return os.path.basename (fu_fn)
+def gf_pn (x_pn) : # (p)ath (n)ame
+  fu_trans = str.maketrans ( '\\','/' )
+  fu_abs_pn = os.path.abspath ( os.path.dirname ( x_pn .translate (fu_trans) ) )
+  return fu_abs_pn .translate (fu_trans)
+def gf_on (x_pn) : # f(o)lder (n)ame
+  fu_pn = gf_ap (x_pn)
+  return os.path.basename (fu_pn)
+def gf_pj (*x_args) : # (p)ath (j)oin
+  fu_trans = str.maketrans ( '\\','/' )
+  return '/' .join (x_args) .translate (fu_trans)
+
+GC_THIS_START_UP_PN = gf_ap ( os.getcwd () )
+
+GC_THIS_EXE_FN = gf_fn (sys.executable)
+GC_THIS_EXE_JN = gf_jn (GC_THIS_EXE_FN)
+GC_THIS_EXE_XN = gf_xn (GC_THIS_EXE_FN)
+GC_THIS_EXE_BN = gf_bn (GC_THIS_EXE_FN)
+GC_THIS_EXE_PN = gf_pn (GC_THIS_EXE_FN)
+GC_THIS_EXE_ON = gf_on (GC_THIS_EXE_FN)
+
+GC_CMD = psutil.Process () .cmdline ()
+def __gaf_asa () : # __gaf -> (__)private (g)lobal priv(a)te (f)unction
+  fu_app_nm = ...
+  fu_script_fn = ...
+  fv_idx4script = 0 # (f)unction (v)ariable
+  for bu2_idx, bu2_it in enumerate (GC_CMD) : # bu2 -> (b)lock val(u)e nested level 2
+    if gf_xn (bu2_it) .lower () in ['py'] :
+      fv_idx4script = bu2_idx
+      fu_script_fn = gf_fn (bu2_it)
+      fu_app_nm = gf_jn (fu_script_fn)
+      break
+  fu_argv = GC_CMD [fv_idx4script+1:] if ( fv_idx4script + 1 ) < len(GC_CMD) else []
+  return fu_app_nm, fu_script_fn, fu_argv
+GC_APP_NM, GC_SCRIPT_FN, GC_ARGV = __gaf_asa ()
+sys.argv = [ GC_SCRIPT_FN ] + GC_ARGV
+
+GC_PYJA_RT   = gf_ap ( gf_os_env ('SC_PYJA_RT') )
+GC_PYJA_HM   = gf_ap ( gf_os_env ('SC_PYJA_HM') )
+GC_PYTHON_HM = gf_ap ( gf_os_env ('SC_PYTHON_HM') )
+GC_MILO_PN   = gf_ap ( gf_os_env ('SC_MILO_PN') )
+
+def gf_banner ( x_leading_space = 0, x_margin_inside = 2 ) :
+  fu_msgs = [
+    '{} {}' .format ( GC_PYJA_NM, GC_APP_NM ),
+    '',
+    'ran on {:04d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}' .format (
+      GC_ST.year, GC_ST.month, GC_ST.day,
+      GC_ST.hour, GC_ST.minute, GC_ST.second,
+    ),
+    'released under the GNU AGPL v3, see <http://www.gnu.org/licenses/>.',
+  ]
+  fu_msl = max ( list ( map ( ( lambda bx2_it : len (bx2_it) ), fu_msgs ) ) ) # max string length
+  fu_ls = x_leading_space # leading space before box
+  fu_mg = x_margin_inside # margin inside box
+  fu_ll = fu_mg + fu_msl + fu_mg # line length inside box
+  def ff2_get_line () : return ' ' * fu_ls + '+' + '-' * fu_ll + '+' # ff2 -> (f)unction inside (f)unction nested level (2)
+  def ff2_get_string (x2_str) :
+    fu2_sl = len (x2_str) # string lentgh
+    fu2_lm = int ( ( fu_ll - fu2_sl ) / 2.0 ) # left margin inside box
+    fu2_rm = fu_ll - ( fu2_sl + fu2_lm ) # right margin inside box
+    return ' ' * fu_ls + ':' + ' ' * fu2_lm + x2_str + ' ' * fu2_rm + ':' 
+  fu_r = [ ff2_get_line () ] + list ( map ( ( lambda bx2_msg : ff2_get_string (bx2_msg) ), fu_msgs ) ) + [ ff2_get_line () ]
+  return fu_r
+
+def gf_ppn (x_pn) : # (p)arent (p)ath (n)ame
+  fu_pn = gf_ap (x_pn)
+  return os.path.dirname (fu_pn)
+
+def gf_ppn_list ( x_pn, x_include_self = False ) :
+  fu_pn = gf_ap (x_pn)
+  fv_list = []
+  fv_prev_ppn = ...
+  if x_include_self :
+    fv_list.append (fu_pn)
+    fv_prev_ppn = fu_pn
+  fv_ppn = os.path.dirname (fu_pn)
+  while fv_ppn != fv_prev_ppn :
+    fv_list.append (fv_ppn)
+    fv_prev_ppn = fv_ppn
+    fv_ppn = os.path.dirname (fv_ppn)
+  return fv_list
+
+def gf_on_list (x_pn) :
+  fu_pn = gf_ap (x_pn)
+  return fu_pn .split ('/')
+def gf_np (x_it) : # (n)orm(p)ath
+  return os.path.normpath (x_it)
+def gp_rt (x_pn) : # gp -> (g)lobal (p)rocedure, rt -> (r)emove (t)ree
+  if gf_il (x_pn) : gp_ul (x_pn)
+  elif gf_id (x_pn) : shutil.rmtree (x_pn)
+def gp_mp (x_pn) : # (m)ake (p)ath
+  if not gf_id (x_pn) : os.makedirs (x_pn)
+def gp_ep (x_pn) : # (e)mpty out (p)ath
+  if not gf_id (x_pn) : return
+  pu_pn, pu_ons, pu_bns = next ( os.walk (x_pn) )
+  for bu2_on in pu_ons :
+    bu2_pn = gf_pj ( pu_pn, bu2_on )
+    gp_rt (bu2_pn)
+  for bu2_bn in pu_bns :
+    bu2_fn = gf_pj ( pu_pn, bu2_bn )
+    gp_ul (bu2_fn)
+
+gf_xi = os.path.exists # e(xi)sts
+gf_id = os.path.isdir # (i)s (d)irectory
+gf_if = os.path.isfile # (i)s (f)ile
+gp_cp = shutil.copy # (c)o(p)y
+gp_ul = os.unlink # (u)n(l)ink
+gp_rf = os.remove # (r)emove (f)ile
+gp_rd = os.rmdir # (r)emove (d)irectory
+
+GC_TCL = tkinter.Tcl ()
+GC_TCL.eval ( """
+  proc gf_tcl_ver {} {
+    return [info tclversion]
+  }
+  proc gf_run_xcmd {args} {
+    set fu_ec [ catch { set fu_result [ exec {*}$args ] } fu_error ]
+    if { $fu_ec } {
+      return [ list $fu_ec $fu_error ]
+    }
+    return [ list $fu_ec $fu_result ]
+  }
+  proc gf_il {x_it} { # (i)s (l)ink
+    return [ expr { [file type $x_it] eq "link" } ]
+  }
+  proc gp_ml { x_link x_target } { # (m)ake (l)ink
+    file link -symbolic $x_link $x_target
+  }
+""")
+GC_TCL_VR = GC_TCL.call ('gf_tcl_ver')
+
+def gf_run_xcmd (x_xcmd) :
+  fu_r = types.SimpleNamespace ()
+  fu_called = GC_TCL.call ( 'gf_run_xcmd', *x_xcmd )
+  fu_r.ru_ec = fu_called [0]
+  fu_r.ru_message = fu_called [1]
+  return fu_r
+def gf_il (x_it) : # (i)s (l)ink
+  return GC_TCL.call ( 'gf_il', x_it ) == 1
+def gp_ml ( x_src, x_link ) : # (m)ake (l)ink
+  return GC_TCL.call ( 'gp_ml', x_link, x_src )
+
+def gf_exception_to_list () :
+  fv_fe = traceback.format_exception ( * sys.exc_info () )
+  del fv_fe [0]
+  fv_fe.reverse ()
+  fv_fe = map ( ( lambda bx2 : bx2.rstrip () ), fv_fe )
+  return list (fv_fe)
+
+def gf_process (x_pid) : return psutil.Process (x_pid)
+def gf_os_available_memory () : return psutil.virtual_memory () .available
+
+def gf_replace_with_px_symbol ( x_pn, x_px_path, x_px_symbol ) :
+  fu_pn = gf_ap (x_pn)
+  if fu_pn .startswith (x_px_path) : return gf_pj ( x_px_symbol, fu_pn [ len (x_px_path) + 1 : ] )
+  else : return fu_pn
+def gf_to_prs (x_pn) : # to (p)yja (r)oot (s)ymbol
+  return gf_replace_with_px_symbol ( x_pn, GC_PYJA_RT, GC_PYJA_RT_SYM )
+def gf_to_phs (x_pn) : # to (p)yja (h)ome (s)ymbol
+  return gf_replace_with_px_symbol ( x_pn, GC_PYJA_HM, GC_PYJA_HM_SYM )
+def gf_to_mps (x_pn) : # to (m)ilo (p)ath (s)ymbol
+  return gf_replace_with_px_symbol ( x_pn, GC_MILO_PN, GC_MILO_PN_SYM )
+
+def gf_list_of_this_memory_maps_so_far () :
+  fu_mm = gf_process (GC_THIS_PID) .memory_maps ()
+  fu_it = list ( map ( lambda bx2_it : bx2_it.path, fu_mm ) )
+  fu_it .sort ()
+  return fu_it
+
+def gp_log_array ( xp_out, x_title, x_array ) :
+  if x_title is not None : xp_out ( '{} :'.format ( x_title ) )
+  for bu2_idx, bu2_it in enumerate ( x_array, start = 1 ) : xp_out ( '  {:2d} => {}' .format ( bu2_idx, bu2_it ) )
+def gp_log_dict ( xp_out, x_title, x_dict ) :
+  if x_title is not None : xp_out ( '{} :'.format ( x_title ) )
+  for bu2_idx, bu2_it in enumerate ( sorted(list(x_dict.keys())), start = 1 ) : xp_out ( '  {:2d} : {} => {}' .format ( bu2_idx, bu2_it, x_dict [bu2_it] ) )
+def gp_log_header ( xp_out, x_header, x_line_width ) :
+  xp_out ( '+' + '-' * x_line_width )
+  xp_out ( ': {}' .format (x_header) )
+  xp_out ( '+' + '-' * x_line_width )
+def gp_log_exception ( xp_out, x_title, x_ex_list ) :
+  if type(x_ex_list) is not list : gp_log_header ( xp_out, 'List of reasons is not given !!!', 60 )
+  else :
+    gp_log_header ( xp_out, x_title, 60 )
+    for bu2_it in x_ex_list : xp_out (bu2_it)
+
+def __gaf_root_logger ( x_level, x_handler, x_format, x_datefmt = '%y%m%d-%H%M%S' ) :
+  fu_root = logging.root
+  fu_root.handlers = []
+  fu_root.setLevel (x_level)
+  if x_handler is not None :
+    x_handler.setFormatter ( logging.Formatter ( x_format, datefmt = x_datefmt ) )
+    fu_root.addHandler (x_handler)
+  return fu_root
+GC_LOG = __gaf_root_logger ( logging.DEBUG, logging.StreamHandler (sys.stdout), '[%(process)06d,%(levelname)-.1s,%(asctime)s] %(message)s' )
+def gp_set_log_level_to_info () : GC_LOG .setLevel (logging.INFO)
+def gp_set_log_level_to_debug () : GC_LOG .setLevel (logging.DEBUG)
+def gf_is_debug () : return GC_LOG .isEnabledFor ( logging.DEBUG )
+
+LgCx = namedtuple ( 'LgCx', [ 'lu_ec', 'lu_ex' ] )
+class __DgExit : # (__)private mo(D)ule (g)lobal
+  __dav_cx = LgCx ( GC_EC_NONE, None )
+  __dav_was_lgcx_processed = False
+  @classmethod
+  def dp_it ( cls, x_cx ) :
+    GC_LOG .debug ( f'Received LgCx ({x_cx.lu_ec})' )
+    if cls.__dav_was_lgcx_processed == False :
+      cls.__dav_was_lgcx_processed = True
+      cls.__dav_cx = x_cx
+      cls.__dap_before_exit ()
+      cls.__dap_exit ()
+  @classmethod
+  def __dap_before_exit (cls) :
+    pu_ec = cls.__dav_cx.lu_ec
+    pu_ex = cls.__dav_cx.lu_ex
+    if pu_ec != GC_EC_SUCCESS or pu_ex is not None :
+      if gf_is_debug () : gp_log_array ( GC_LOG .debug, 'Files of this memory maps', gf_list_of_this_memory_maps_so_far () )
+      gp_log_array ( GC_LOG .info,  'Python search paths', sys.path )
+    if pu_ex is not None : gp_log_exception ( GC_LOG .error, 'Following error occurs !!!', pu_ex )
+    if pu_ec != GC_EC_SUCCESS and pu_ex is None : gp_log_header ( GC_LOG .error, 'Unknown error occurs !!!', 60 )
+    if pu_ec == GC_EC_NONE : GC_LOG .error ( 'Undefined exit code (GC_EC_NONE), check your logic !!!' )
+    elif pu_ec == GC_EC_SHUTDOWN : GC_LOG .info ( 'Exit code from shutdown like ctrl+c, ...' )
+    elif pu_ec < 0 : GC_LOG .error ( f'Negative exit code ({pu_ec}), should consider using a positive value !!!' )
+    else : GC_LOG .info ( 'Exit code => {}' .format (pu_ec) )
+    GC_LOG .info ( 'Elapsed {} ...' .format ( datetime.now () - GC_ST ) )
+  @classmethod
+  def __dap_exit (cls) :
+    pu_ec = cls.__dav_cx.lu_ec
+    if pu_ec == GC_EC_NONE : os._exit (GC_EC_ERROR)
+    elif pu_ec == GC_EC_SHUTDOWN : pass
+    elif pu_ec < 0 : os._exit (GC_EC_ERROR)
+    else : os._exit (pu_ec)
+def gp_request_exit ( x_ec, x_ex_list = None ) : __DgExit .dp_it ( LgCx ( x_ec, x_ex_list ) )
+
+#
+# Main Skeleton
+#
+
+class DRun :
+  __dav_ec = GC_EC_NONE
+  __dav_ex_list = None
+  @classmethod
+  def __dap_begin (cls) :
+    print ( "\n" .join ( gf_banner () ) )
+    GC_LOG .debug ( f'Pyja name => {GC_PYJA_NM}' )
+    if GC_PYJA_NM != gf_os_env ('SC_PYJA_NM') : raise Exception ( 'Invalid Pyja name !!!' )
+    GC_LOG .debug ( f'Pyja creation date => {GC_PYJA_CD}' )
+    if not gf_str_is_valid_date ( GC_PYJA_CD, '%Y.%m.%d' ) : raise Exception ( 'Pyja create date is not invalid !!!' )
+    GC_LOG .debug ( f'Pyja version => {GC_PYJA_V2}' )
+    if GC_PYJA_VR != gf_os_env ('SC_PYJA_VR') : raise Exception ( 'Invalid Pyja version !!!' )
+    GC_LOG .info  ( f'Pyja root ({GC_PYJA_RT_SYM}) => {GC_PYJA_RT}' )
+    GC_LOG .info  ( f'Pyja home ({GC_PYJA_HM_SYM}) => { gf_to_prs (GC_PYJA_HM) }' )
+    GC_LOG .info  ( f'Milo path ({GC_MILO_PN_SYM}) => { gf_to_phs (GC_MILO_PN) }' )
+    GC_LOG .debug ( f'Python home => {GC_PYTHON_HM}' )
+    GC_LOG .debug ( f'Total CPU => {GC_TOTAL_CPU}' )
+    GC_LOG .debug ( f'Total memory => {GC_TOTAL_MEMORY:,d} bytes' )
+    GC_LOG .debug ( f'Available memory => {gf_os_available_memory():,d} bytes' )
+    GC_LOG .debug ( f'Computer name => {GC_HOST_NM}' )
+    GC_LOG .debug ( f'Current user => {GC_CUSR}' )
+    GC_LOG .debug ( f'Process ID => {GC_THIS_PID}' )
+    GC_LOG .debug ( f'Executable file => {GC_THIS_EXE_FN}' )
+    GC_LOG .info  ( f'Start up path => { gf_to_phs (GC_THIS_START_UP_PN) }' )
+    GC_LOG .info  ( f'Script file => { gf_to_mps (GC_SCRIPT_FN) }' )
+    gp_log_array ( GC_LOG .debug, 'Paths', GC_OS_ENV_PATHS )
+    if len(GC_CMD) > 0 : gp_log_array ( GC_LOG .debug, 'Command', GC_CMD )
+    if len(GC_ARGV) > 0 : gp_log_array ( GC_LOG .info, 'Arguments', GC_ARGV )
+  @classmethod
+  def dp_it (cls) :
+    try :
+      cls.__dap_begin ()
+      DBody .dp_it ()
+      cls.__dav_ec = GC_EC_SUCCESS
+    except :
+      cls.__dav_ec = GC_EC_ERROR
+      cls.__dav_ex_list = gf_exception_to_list ()
+    finally :
+      gp_request_exit ( cls.__dav_ec, cls.__dav_ex_list )
+
+#
+# Your Source
+#
+
+from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QFontMetrics
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QWidget
+
+class WWiggly (QWidget) :
+  WC_SINE_TBL = [ 0, 38, 71, 92, 100, 92, 71, 38, 0, -38, -71, -92, -100, -92, -71, -38 ]
+  def __init__ (self) :
+    super () .__init__ ()
+    self .wn_init ()
+  def wn_init (self) :
+    nu_fnt = self .font ()
+    nu_fnt .setPointSize ( nu_fnt .pointSize () + 20 )
+    self .setFont (nu_fnt)
+    self.wv_step = 0
+    self .startTimer (60)
+  def timerEvent ( self, x_ev ) :
+    self.wv_step += 1
+    self .update ()
+  def paintEvent ( self, x_ev ) :
+    nu_metrics = QFontMetrics ( self .font () )
+    nv_x = int ( ( self .width () - nu_metrics .width (self.wv_text) ) / 2 )
+    nu_y = int ( ( self .height () + nu_metrics .ascent () - nu_metrics .descent () ) / 2 )
+    nu_color = QColor ()
+    nu_painter = QPainter ()
+    nu_painter .begin (self)
+    nu_tbl_sz = len (self.WC_SINE_TBL)
+    for bu2_i, bu2_ch in enumerate (self.wv_text) :
+      bu2_idx = ( self.wv_step + bu2_i ) % nu_tbl_sz
+      nu_color .setHsv ( ( nu_tbl_sz - 1 - bu2_idx ) * nu_tbl_sz, 255, 191 )
+      nu_painter .setPen (nu_color)
+      nu_painter .drawText ( nv_x, nu_y - int ( ( self.WC_SINE_TBL [bu2_idx] * nu_metrics .height () ) / 400 ), bu2_ch )
+      nv_x += nu_metrics .width (bu2_ch)
+    nu_painter .end ()
+  def won_set_text ( self, x_text ) :
+    GC_LOG .info ( f'Setting text => {x_text}' )
+    self.wv_text = x_text
+
+class WMain (QMainWindow) :
+  def __init__ (self) :
+    super () .__init__ ()
+    self .wn_init ()
+  def wn_init (self) :
+    self .setWindowTitle (GC_APP_NM)
+    self .resize ( 460, 145 )
+    self.wu_cw = QWidget ()
+    self.wu_lo = QVBoxLayout ()
+    self.wu_wiggly = WWiggly ()
+    self.wu_le = QLineEdit ()
+    self.wu_le .textChanged .connect ( self.wu_wiggly.won_set_text )
+    self.wu_le .setText ('Hello Pyja !')
+    for bu2_it in [ self.wu_wiggly, self.wu_le ] : self.wu_lo .addWidget (bu2_it)
+    self.wu_cw .setLayout (self.wu_lo)
+    self .setCentralWidget (self.wu_cw)
+    self .show ()
+    self .raise_ ()
+
+class DBody :
+  @classmethod
+  def dp_it (cls) :
+    GC_QAPP .setStyle ('fusion')
+    pu_w = WMain ()
+    GC_QAPP .exec_ ()
+
+class OStart :
+  @classmethod
+  def main (cls) :
+    # gp_set_log_level_to_info ()
+    DRun .dp_it ()
+  
+if __name__ == '__main__':
+  OStart .main ()
