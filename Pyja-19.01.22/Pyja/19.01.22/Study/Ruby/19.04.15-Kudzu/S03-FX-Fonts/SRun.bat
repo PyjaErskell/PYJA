2ddb57bbf580bc8c948ba994af2fcaaf30c14cfe
Pyja Erskell <pyja.erskell@gmail.com>
2019-04-29 13:40:30 +0900

    On Windows 64 bit, JavaFX example from CRuby

--- Pyja-19.01.22/Pyja/19.01.22/Study/Ruby/19.04.15-Kudzu/S03-FX-Fonts/SRun.bat
+++ Pyja-19.01.22/Pyja/19.01.22/Study/Ruby/19.04.15-Kudzu/S03-FX-Fonts/SRun.bat
@@ -20,12 +20,9 @@ FOR %%F IN (%SC_PYJA_HM%) DO SET SC_PYJA_VR=%%~NXF
 POPD
 
 CALL "%SC_PYJA_HM%\Config\SSet-KAPA.bat"
-CALL "%SC_PYJA_HM%\Config\Anaconda\x64\SSet-Anaconda-5.1.0.bat"
-CALL "%SC_PYJA_HM%\Config\RubyInstaller\x64\SSet-Ruby-2.3.3.bat"
-CALL "%SC_PYJA_HM%\Config\QtJambi\x64\SSet-QtJambi-4.8.7.bat"
-CALL "%SC_PYJA_HM%\Config\JDK\x64\SSet-JDK-8-192.bat"
+rem CALL "%SC_TONO_HM%\SSet-x32.bat"
+CALL "%SC_TONO_HM%\SSet-x64.bat"
 
-SET SC_JAVA_XMX=-Xmx9g
 
 SET JAVA_HOME=%SC_J8_HM%
 SET PYTHONHOME=%SC_PYTHON_HM%
---
+++ Pyja-19.01.22/Pyja/19.01.22/Study/Ruby/19.04.15-Kudzu/S03-FX-Fonts/SSet-x64.bat
@@ -0,0 +1,6 @@
+CALL "%SC_PYJA_HM%\Config\Anaconda\x64\SSet-Anaconda-5.1.0.bat"
+CALL "%SC_PYJA_HM%\Config\RubyInstaller\x64\SSet-Ruby-2.3.3.bat"
+CALL "%SC_PYJA_HM%\Config\QtJambi\x64\SSet-QtJambi-4.8.7.bat"
+CALL "%SC_PYJA_HM%\Config\JDK\x64\SSet-JDK-8-192.bat"
+
+SET SC_JAVA_XMX=-Xmx9g
