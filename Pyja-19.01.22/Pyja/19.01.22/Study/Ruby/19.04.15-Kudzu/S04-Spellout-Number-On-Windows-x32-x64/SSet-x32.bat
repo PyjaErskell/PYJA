93f6008f176bad5970bce3b76f6b435800aa0215
Pyja Erskell <pyja.erskell@gmail.com>
2019-04-29 17:27:12 +0900

    On macOS 64 bit and Windows 32 64 bit, PyCall and Rjb example from CRuby

---
+++ Pyja-19.01.22/Pyja/19.01.22/Study/Ruby/19.04.15-Kudzu/S04-Spellout-Number-On-Windows-x32-x64/SSet-x32.bat
@@ -0,0 +1,6 @@
+CALL "%SC_PYJA_HM%\Config\Anaconda\x32\SSet-Anaconda-2.3.0.bat"
+CALL "%SC_PYJA_HM%\Config\RubyInstaller\x32\SSet-Ruby-2.3.3.bat"
+CALL "%SC_PYJA_HM%\Config\QtJambi\x32\SSet-QtJambi-4.8.6.bat"
+CALL "%SC_PYJA_HM%\Config\JDK\x32\SSet-JDK-8-212.bat"
+
+SET SC_JAVA_XMX=-Xmx1400m
