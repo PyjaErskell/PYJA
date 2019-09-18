3ef04c4a92c79743df64dc07f369ac5a7acb5a36
Pyja Erskell <pyja.erskell@gmail.com>
2019-09-18 17:15:58 +0900

    S01-Pass-Nashorn-To-Groovy

--- Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.groovy
+++ Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.groovy
@@ -2,3 +2,13 @@ GC_NH .print ( "Call Nashorn's print from Groovy : Hello ${ 111 + 999 }\n" )
 println GC_NH.gu_string
 println "Nashorn function's class name => ${ GC_NH .gf_hello .getClass () }"
 println GC_NH .gf_hello .call ( GC_NH, 'Electrika' )
+GC_NH .getClass () .metaClass .methodMissing = { x_name, x_args -> GC_NH ."$x_name" .call ( GC_NH, * x_args ) }
+println GC_NH .gf_hello ('^^')
+println GC_NH .gf_sum ( 1, 20, 3000000000000000 )
+
+// Call Nashorn's print from Groovy : Hello 1110
+// Hello, this is Nashorn string
+// Nashorn function's class name => class jdk.nashorn.api.scripting.ScriptObjectMirror
+// Hello Electrika !!!
+// Hello ^^ !!!
+// 3000000000000021
--- Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.js
+++ Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.js
@@ -4,4 +4,5 @@ gu_string = "Hello, this is Nashorn string";
 function gf_hello (x_name) {
   return "Hello " + x_name + " !!!";
 }
+function gf_sum ( x_1, x_2, x_3 ) { return new java.lang.Long ( x_1 + x_2 + x_3 ); }
 GC_GR .eval ( new Packages.java.io.FileReader ( ( Packages.java.lang.System ) .getenv ("SC_TONO_HM") + Packages.java.io.File.separator + "SToa.groovy" ) );
