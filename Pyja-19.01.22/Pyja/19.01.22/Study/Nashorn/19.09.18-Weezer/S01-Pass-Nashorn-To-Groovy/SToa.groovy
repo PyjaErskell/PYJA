76140141ebeea1cc9884db4adc108149b183dec1
Pyja Erskell <pyja.erskell@gmail.com>
2019-09-19 09:12:59 +0900

    S01-Pass-Nashorn-To-Groovy

--- Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.groovy
+++ Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.groovy
@@ -1,14 +1,27 @@
-GC_NH .print ( "Call Nashorn's print from Groovy : Hello ${ 111 + 999 }\n" )
-println GC_NH.gu_string
-println "Nashorn function's class name => ${ GC_NH .gf_hello .getClass () }"
-println GC_NH .gf_hello .call ( GC_NH, 'Electrika' )
-GC_NH .getClass () .metaClass .methodMissing = { x_name, x_args -> GC_NH ."$x_name" .call ( GC_NH, * x_args ) }
-println GC_NH .gf_hello ('^^')
-println GC_NH .gf_sum ( 1, 20, 3000000000000000 )
+gf_sys_props = { x_key ->
+  final fu_it = System .getProperty (x_key)
+  if ( fu_it == null ) { throw new NullPointerException ( "System property not found => ${x_key}" ) }
+  fu_it
+}
+println "Java version => ${ gf_sys_props 'java.version' }"
+println "Groovy version => ${GroovySystem.version}"
+println "GC_GR's class => ${GC_GR.class}"
+println "GC_NA's class => ${ GC_NA .getClass () }"
+GC_NA .print ( "Call Nashorn's print from Groovy : Hello ${ 111 + 999 }\n" )
+println GC_NA.gu_string
+println "Nashorn function's class => ${ GC_NA .gf_hello .getClass () }"
+println GC_NA .gf_hello .call ( GC_NA, 'Electrika' )
+GC_NA .getClass () .metaClass .methodMissing = { x_name, x_args -> GC_NA ."$x_name" .call ( GC_NA, * x_args ) }
+println GC_NA .gf_hello ('^^')
+println GC_NA .gf_sum ( 1, 20, 3000000000000000 )
 
+// Java version => 1.8.0_202
+// Groovy version => 2.5.5
+// GC_GR's class => class org.codehaus.groovy.jsr223.GroovyScriptEngineImpl
+// GC_NA's class => class jdk.nashorn.api.scripting.ScriptObjectMirror
 // Call Nashorn's print from Groovy : Hello 1110
 // Hello, this is Nashorn string
-// Nashorn function's class name => class jdk.nashorn.api.scripting.ScriptObjectMirror
+// Nashorn function's class => class jdk.nashorn.api.scripting.ScriptObjectMirror
 // Hello Electrika !!!
 // Hello ^^ !!!
 // 3000000000000021
--- Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.js
+++ Pyja-19.01.22/Pyja/19.01.22/Study/Nashorn/19.09.18-Weezer/S01-Pass-Nashorn-To-Groovy/SToa.js
@@ -1,8 +1,9 @@
 var GC_GR = new Packages.javax.script.ScriptEngineManager () .getEngineByName("Groovy");
-GC_GR .put ( "GC_NH", this );
+GC_GR .put ( "GC_GR", GC_GR );
+GC_GR .put ( "GC_NA", this );
 gu_string = "Hello, this is Nashorn string";
 function gf_hello (x_name) {
   return "Hello " + x_name + " !!!";
 }
 function gf_sum ( x_1, x_2, x_3 ) { return new java.lang.Long ( x_1 + x_2 + x_3 ); }
-GC_GR .eval ( new Packages.java.io.FileReader ( ( Packages.java.lang.System ) .getenv ("SC_TONO_HM") + Packages.java.io.File.separator + "SToa.groovy" ) );
+GC_GR .eval ( new java.io.FileReader ( ( java.lang.System ) .getenv ("SC_TONO_HM") + java.io.File.separator + "SToa.groovy" ) );
