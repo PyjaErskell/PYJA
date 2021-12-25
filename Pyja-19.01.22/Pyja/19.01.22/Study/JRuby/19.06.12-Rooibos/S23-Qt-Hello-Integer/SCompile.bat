e94e0c8f39dcdacf52556f6c4b5764952b941ae9
Pyja Erskell <pyja.erskell@gmail.com>
2021-12-25 20:09:35 +0900

    +

---
+++ Pyja-19.01.22/Pyja/19.01.22/Study/JRuby/19.06.12-Rooibos/S23-Qt-Hello-Integer/SCompile.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+SETLOCAL
+
+SET PATH=%SystemRoot%\system32
+
+SET SC_THIS_BN=%~NX0
+SET SC_THIS_JN=%~N0
+
+PUSHD "%~DP0"
+CD "%~DP0"
+SET SC_TONO_HM=%CD%
+CD %SC_TONO_HM%\..
+SET SC_MILO_PN=%CD%
+CD "%SC_MILO_PN%\..\..\.."
+SET SC_PYJA_HM=%CD%
+CD "%SC_PYJA_HM%\.."
+SET SC_PYJA_RT=%CD%
+FOR %%F IN (%SC_PYJA_RT%) DO SET SC_PYJA_NM=%%~NXF
+FOR %%F IN (%SC_PYJA_HM%) DO SET SC_PYJA_VR=%%~NXF
+POPD
+
+CALL "%SC_PYJA_HM%\Config\SSet-KAPA.bat"
+CALL "%SC_PYJA_HM%\Config\JDK\x64\SSet-JDK-8.bat"
+CALL "%SC_PYJA_HM%\Config\JRuby\SSet-JRuby-9.2.5.0.bat"
+
+SET JAVA_HOME=%SC_J8_HM%
+SET SC_JAVA_XMX=-Xmx9g
+
+SET JRUBY_HOME=%SC_JRUBY_HM%
+SET RUBYLIB=%SC_TONO_HM%
+
+SET SC_PATH=%PATH%
+
+"%SC_J8_X_FN%" -Dfile.encoding=UTF-8 -cp "%SC_JRUBY_JAR_FN%" org.jruby.Main "%SC_TONO_HM%\src\jruby\compile\SToa.rb" %*
+
+ENDLOCAL
